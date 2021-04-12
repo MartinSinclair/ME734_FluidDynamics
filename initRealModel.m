539786341c9732548fce35cf16fcd2d157499ffb
MartinSinclair <sinclairmartin36@gmail.com>
2021-04-12 00:04:09 -0400

    update with first pass at realistic model

---
+++ initRealModel.m
@@ -0,0 +1,41 @@
+%% set sample time
+dt = 1/1000;
+
+%% set flow rates
+Q1 = 200;
+Q2 = 200;
+Q3 = 200;
+Q4 = 200;
+
+%% pipe widths
+W.A = 1;
+W.B = 1;
+W.C = 1;
+W.D = 1;
+W.E = 1;
+W.F = 1;
+W.G = 1;
+
+%% pipe heights
+H.A = 1;
+H.B = 1;
+H.C = 1;
+H.D = 1;
+H.E = 1;
+H.F = 1;
+H.G = 1;
+
+%% pipe length
+L.A = 10;
+L.B = 10;
+L.C = 10;
+L.D = 10;
+L.E = 10;
+L.F = 10;
+L.G = 10;
+
+%% droplet in system
+seed_1 = 0;
+seed_2 = 1;
+seed_3 = 2;
+seed_4 = 3;

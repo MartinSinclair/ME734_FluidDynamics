bb0fbaa8c256eb7bab03ccdb6be9233fef3a5f7a
MartinSinclair <sinclairmartin36@gmail.com>
2021-04-14 23:53:23 -0400

    update with first pass at working monte carlo for simple system

---
+++ initMonteCarlo.m
@@ -0,0 +1,36 @@
+%% set sample time
+dt = 1*10^-4;
+
+%% set flow rates
+Q1 = 200;
+Q2 = 200;
+
+%% pipe widths
+W_A = 1;1.5;
+W_B = 1;0.5;
+W_C = 1;
+
+
+%% pipe heights
+H_A = 1;1.5;
+H_B = 1;0.5;
+H_C = 1;
+
+%% pipe length
+L_A = 10;
+L_B = 10;
+L_C = 10;
+
+
+%% droplet in system
+seed_1 = 0;
+seed_2 = 1;
+
+
+%% rates for each input
+rate_1 = 0.01;
+rate_2 = 0.01;
+
+% phase
+phase_1 = 0;
+phase_2 = 0.005;
